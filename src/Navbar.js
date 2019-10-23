6d28802d1f0e5825b1335fbe5f99be515bdd874a
Manuela Cardoso <manuelapcardoso@gmail.com>
2019-10-23 16:28:56 +0100

    add base navbar

--- src/Navbar.js
+++ src/Navbar.js
@@ -1,12 +1,26 @@
 import React from 'react';
-
+import { NavLink } from 'react-router-dom';
 
 const Navbar = () =>{
    return (
-      <p>This is navbar</p>
+      <nav> 
+         <ul>
+            <li>
+               <NavLink exact to='/' >Homepage</NavLink>
+            </li>
+            <li>
+               <NavLink exact to='/quiz' >Quiz</NavLink>
+            </li>
+            <li>
+               <NavLink to='/result' >Result</NavLink>
+            </li>
+            <li>
+               <NavLink to='/about' >About us</NavLink>
+            </li>
+         </ul>
+      </nav>
       
    )
 }
-
 
 export default Navbar;

b11eaf81a78a53f566056806aee95666c0f9c2a4
Manuela Cardoso <manuelapcardoso@gmail.com>
2019-10-31 12:29:47 +0000

    Clean up the files a little bit

--- src/Btn.js
+++ src/Btn.js
@@ -1,17 +1,16 @@
 import React from 'react';
-import {Link} from 'react-router-dom'
+import { Link } from 'react-router-dom'
 import './Btn.css'
 
-
-const Btn = ({destination, text}) =>{
- console.log(destination);
+const Btn = ({ destination, text }) => {
   return (
     <div className ="BtnContainer"> 
-      <Link to={`${destination}`}> <button className="btn default">{text}</button> 
-  </Link>
+      <Link to={`${destination}`}> 
+        <button className="btn default">{text}</button> 
+      </Link>
   </div>
     )
   };
  
  
- export default Btn;
+export default Btn;

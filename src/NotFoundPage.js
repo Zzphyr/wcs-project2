9619a34c9969a321b9ae4c048105f82027f3186c
Manuela Cardoso <manuelapcardoso@gmail.com>
2019-11-14 13:26:38 +0000

    Another try deploy Github v3, styled 404page

--- src/NotFoundPage.js
+++ src/NotFoundPage.js
@@ -5,7 +5,7 @@ import { Link } from 'react-router-dom';
 class NotFoundPage extends React.Component{
    render(){
       return (
-         <div style={{textAlign:"center"}}>
+         <div style={{textAlign:"center", position:"relative", top:"20vh"}}>
             <h1>Ops! This is not the page you are looking for!</h1>
             <h3>Now dog is sad it only exists in his imagination...</h3>
             <img src='https://i.imgur.com/ZSzQ3W4.png?1' alt='' />

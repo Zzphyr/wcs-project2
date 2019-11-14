69b83f4b4ea5c175c28e69cc44661b8deee77402
Manuela Cardoso <manuelapcardoso@gmail.com>
2019-11-14 12:10:18 +0000

    Updates

---
+++ precache-manifest.df33a1dd2ee73391617258d111801693.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "10c7d5defcfc1b0f78bf815bd8cbc81c",
+    "url": "/Zzphyr/wcs-project2/tree/master/public/index.html/index.html"
+  },
+  {
+    "revision": "a4c7586b757eecdf5b15",
+    "url": "/Zzphyr/wcs-project2/tree/master/public/index.html/static/css/main.6d8e9670.chunk.css"
+  },
+  {
+    "revision": "90416eaf39a21f49ee31",
+    "url": "/Zzphyr/wcs-project2/tree/master/public/index.html/static/js/2.633d80aa.chunk.js"
+  },
+  {
+    "revision": "a4c7586b757eecdf5b15",
+    "url": "/Zzphyr/wcs-project2/tree/master/public/index.html/static/js/main.4049c6a2.chunk.js"
+  },
+  {
+    "revision": "59f91957e2a9f47807f6",
+    "url": "/Zzphyr/wcs-project2/tree/master/public/index.html/static/js/runtime-main.dad132a6.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/Zzphyr/wcs-project2/tree/master/public/precache-manifest.8f73b06d71f76b325f7a2ca51ae01336.js"
+  "/Zzphyr/wcs-project2/tree/master/public/index.html/precache-manifest.df33a1dd2ee73391617258d111801693.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/Zzphyr/wcs-project2/tree/master/public/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/Zzphyr/wcs-project2/tree/master/public/index.html/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });

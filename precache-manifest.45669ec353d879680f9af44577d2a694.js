a0659f1b8e0b5b83b2e2ae1b7b617123d7363516
Manuela Cardoso <manuelapcardoso@gmail.com>
2019-11-14 12:03:14 +0000

    Updates

---
+++ precache-manifest.45669ec353d879680f9af44577d2a694.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8494fe11f6190bf5d8e346340382c11b",
+    "url": "/Zzphyr/wcs-project2/index.html"
+  },
+  {
+    "revision": "1a4cb5a14541ee7f57a2",
+    "url": "/Zzphyr/wcs-project2/static/css/main.6d8e9670.chunk.css"
+  },
+  {
+    "revision": "90416eaf39a21f49ee31",
+    "url": "/Zzphyr/wcs-project2/static/js/2.633d80aa.chunk.js"
+  },
+  {
+    "revision": "1a4cb5a14541ee7f57a2",
+    "url": "/Zzphyr/wcs-project2/static/js/main.45a3df4a.chunk.js"
+  },
+  {
+    "revision": "ef89b2d5885876b9e0e3",
+    "url": "/Zzphyr/wcs-project2/static/js/runtime-main.0a46cc8f.js"
+  }
+]);

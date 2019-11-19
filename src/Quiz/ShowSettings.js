e3b72f880d777ab44e6be9baa783b572a7060799
Toria <tzimms7@gmail.com>
2019-11-19 16:54:04 +0000

    timerehh

--- src/Quiz/ShowSettings.js
+++ src/Quiz/ShowSettings.js
@@ -3,10 +3,10 @@ import './ShowSettings.css'
 
 const ShowSettings = ({ chosenCat, chosenDif }) => {
    return (
-      <React.Fragment className="show-settings">
+      <div className="show-settings">
          <p>Category: {chosenCat.name === "Any Category" ? "Any" : chosenCat.name}</p>
          <p>Difficulty: {chosenDif.difficulty === "Any Difficulty" ? "Any" : chosenDif.difficulty}</p>
-      </React.Fragment>
+      </div>
    )
 }
 

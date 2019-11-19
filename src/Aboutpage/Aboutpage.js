9e6ddf08e523df9138feddfc05027fd7b908db42
Manuela Cardoso <manuelapcardoso@gmail.com>
2019-11-19 15:59:17 +0000

    Re-style about page

--- src/Aboutpage/Aboutpage.js
+++ src/Aboutpage/Aboutpage.js
@@ -12,7 +12,7 @@ const Aboutpage = () => {
          <People />
          <article className="project-description">
             <p>App developed for Wild Code School's second group project in React Web Dev course.</p>
-            <p>Here we aimed to apply what we learned in just one month. More than a showcase of knowledge, we wanted to train and practice with an ambicious project for newcomers... and to provide an enjoyable experience to you, dear visitor!</p>  
+            <p>Here we aimed to apply what we learned in one month. We wanted to train and practice with an ambicious project for newcomers, not just as a showcase of knowledge... and to provide an enjoyable experience to you, dear visitor!</p>  
          </article>
       </main>
    );
